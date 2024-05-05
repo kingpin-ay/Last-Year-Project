4b999f14c5fef315906ffa3e09b9e85eb6b46941
ayush-eubrics01 <ayush_mondal@eubrics.com>
2024-05-05 11:56:58 +0530

    feat: initial scafolding done of the project

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ai Storage Optimizer",
+  description:
+    "This is a application to visulize the optimized data saved on change transactions",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
